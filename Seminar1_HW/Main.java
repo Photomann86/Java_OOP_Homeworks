af1c6ad281e14353bbcc3a52b44076ab917a93a5
Photomann86 <kunzler555@gmail.com>
2023-04-22 00:09:23 +0500

    создан объект Main

---
+++ Seminar1_HW/Main.java
@@ -0,0 +1,19 @@
+package Seminar1_HW;
+
+import java.utils.Arraylist;
+import java.utils.List;
+public class Main{
+    public static void main(String[] args) {
+        //Product product1 = new Product();
+        List<Product> products = new ArrayList<>();
+        VendingMachine VendingMachine = new VendingMachine(products);
+        Chocolate chocolate1 = new Chocolate("Bounty", "Nestle", 85.5, 35);
+        Chocolate chocolate2 = new Chocolate();
+        Chocolate chocolate3 = new chocolate();
+        Chocolate chocolate4 = new chocolate();
+        Chocolate chocolate5 = new chocolate();
+        Chocolate chocolate6 = new chocolate();
+        Chocolate chocolate7 = new chocolate();
+        Chocolate chocolate8 = new chocolate();
+    }
+}

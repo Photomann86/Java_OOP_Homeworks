275d45d2474b013cf56a1f3d4eef015e8af220ed
Photomann86 <kunzler555@gmail.com>
2023-04-23 20:56:55 +0500

    прописан класс молока

--- Seminar1_HW/BottleOfMilk.java
+++ Seminar1_HW/BottleOfMilk.java
@@ -1,5 +1,18 @@
 package Seminar1_HW;
 
 public class BottleOfMilk extends Product {
+    double fat;
+    double volume;
+
+    public BottleOfMilk(String name, String brand, double price, double volume, double fat) {
+        super(name, price);
+        this.fat = fat;
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", volume : %f, fat: %f", this.volume, this.fat);
+    }
     
 }

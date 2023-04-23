b645971def68adfffb1b8d931a7bf97ce5932077
Photomann86 <kunzler555@gmail.com>
2023-04-23 22:58:07 +0500

    добавлен новый продукт в вендинговый аппарат

--- Seminar1_HW/Main.java
+++ Seminar1_HW/Main.java
@@ -5,6 +5,7 @@ public class Main{
         vm.addProduct(new BottleOfWater("минералка", "Borjomi", 70.5, 0.5));
         vm.addProduct(new BottleOfMilk("молоко", "Простоквашино", 70.99, 0.9, 3.2));
         vm.addProduct(new Chocolate("Аленка", "Красный октябрь", 40.33, 35));
+        vm.addProduct(new BubbleGum("жевательная резинка", "ORBIT", 56.50, 10));
 
         System.out.println(vm);
     }

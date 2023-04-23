19e5e3579366e2bca008f02e536cc6a9232d52d5
Photomann86 <kunzler555@gmail.com>
2023-04-23 21:01:33 +0500

    описан класс воды

--- Seminar1_HW/BottleOfWater.java
+++ Seminar1_HW/BottleOfWater.java
@@ -1,4 +1,15 @@
 package Seminar1_HW;
 public class BottleOfWater extends Product{
+    
+    double volume;
+
+    public BottleOfWater(String name, String brand, double price, double volume) {
+        super(name, price);
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", volume : %f", this.volume);
 
 }

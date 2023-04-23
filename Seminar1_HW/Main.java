2dcb06ccb5b5a11b891b0aedc73ed80359737eaa
Photomann86 <kunzler555@gmail.com>
2023-04-23 21:05:22 +0500

    дописан главный файл

--- Seminar1_HW/Main.java
+++ Seminar1_HW/Main.java
@@ -1,6 +1,11 @@
 package Seminar1_HW;
 public class Main{
     public static void main(String[] args) {
-        
+        VendingMachine vm = new VendingMachine();
+        vm.addProduct(new BottleOfWater("минералка", "Borjomi", 0.5, 70));
+        vm.addProduct(new BottleOfMilk("молоко", "Простоквашино", 70, 0.9, 3.2));
+        vm.addProduct(new Chocolate("Аленка", "Красный октябрь", 40, 35));
+
+        System.out.println(vm);
     }
 }
